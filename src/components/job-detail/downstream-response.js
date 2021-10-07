7d027f1a00853ea43346192c7fb17ab948b38a03
kittys693 <46304924+kittys693@users.noreply.github.com>
2021-10-07 16:23:17 -0500

    Update downstream-response.js
    sravika change

--- src/components/job-detail/downstream-response.js
+++ src/components/job-detail/downstream-response.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React from "react";
+import "./job-detail.scss";
+import { Row } from "react-bootstrap";
+import { jsonify } from "../../common/utils";
 
-
-const DownstreamResponse = () => {
-    return <></>;
-}
-
+const DownstreamResponse = (props) => {
+  return (
+    <Row className="card-body">
+      <h5 class="card-title">Downstream Response</h5>
+      <pre id="job-downstream-response" class="code">
+        <code>{jsonify(props?.response)}</code>
+      </pre>
+    </Row>
+  );
+};
 
 export default DownstreamResponse;
