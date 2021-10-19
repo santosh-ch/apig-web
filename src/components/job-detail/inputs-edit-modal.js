19d43e2bd53e40841e820d7ccdd108c926d71d96
Santosh ch <cherukurisantosh5@gmail.com>
2021-10-19 18:25:04 +0530

    Edit inputs

--- src/components/job-detail/inputs-edit-modal.js
+++ src/components/job-detail/inputs-edit-modal.js
@@ -3,6 +3,20 @@ import { Modal, Button } from 'react-bootstrap';
 import PropTypes from 'prop-types';
 
 const InputsEditModal = (props) => {
+
+    function addInput() {
+        return (<div className="edit-input-new">
+            <label>Input: </label>
+            <input className="edit-input-new-key" style={{ marginLeft: '8px', paddingLeft: '8px' }} type="text" placeholder="Input key" />
+            <label style={{ marginLeft: '16px' }}>Value: </label>
+            <input className="edit-input-new-val" style={{ marginLeft: '8px', paddingLeft: '8px' }} type=" text" placeholder="Input value" />
+            <i className="material-icons" style={{ cursor: 'pointer' }} onClick={removeEditInput}>delete</i>
+        </div>);
+    }
+
+    var removeEditInput = function (e) {
+        e.target.parent().remove();
+    }
     return (
         <Modal
             {...props}
@@ -23,10 +37,20 @@ const InputsEditModal = (props) => {
                 <div style={{ marginBottom: '24px' }} className="form-group">
                     <p><strong>Edit Inputs</strong></p>
                     <div id="edit-input-container">
+                        {props?.data['apig-payload']['inputs'] && Object.keys(props?.data['apig-payload']['inputs']).map( key =>(
+                            <div className="edit-input-new" key={key}>
+                            <label>Input: </label>
+                            <input id={'job-input-key' + key.trim()} value={key} className="edit-input-new-key" style={{ marginLeft: '8px', paddingLeft: '8px' }} type="text" />
+                            <label style={{ marginLeft: '16px' }}>Value: </label>
+                            <input id={"job-input-value-" + key.trim()} value={ props?.data['apig-payload']['inputs'][key]} className="edit-input-new-val" style={{ marginLeft: '8px', paddingLeft: '8px' }} type="text" />
+                            <Button variant="primary" style={{ marginLeft: '16px' }} onClick={removeEditInput}>delete</Button>
+                        </div>
+                        ))}
                     </div>
                 </div>
 
-                <span id="edit-add-input-btn" className="btn btn-primary" style={{ width: '100%', marginBottom: '24px' }}>Add Input</span>
+                <span id="edit-add-input-btn" className="btn btn-primary"
+                    style={{ width: '100%', marginBottom: '24px' }} onClick={() => addInput}>Add Input</span>
             </Modal.Body>
             <Modal.Footer>
                 <Button variant="secondary" onClick={() => { props.onHide; }}>Save Changes</Button>
